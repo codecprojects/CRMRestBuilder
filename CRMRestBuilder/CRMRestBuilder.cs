7d18d67b4a3a91dd65274d492f1d4fcb5b63d442
Giles Forde <S00083446@mail.itsligo.ie>
2019-05-13 09:38:50 +0100

    Created function to add an Account entity

--- CRMRestBuilder/CRMRestBuilder.cs
+++ CRMRestBuilder/CRMRestBuilder.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CRMRestBuilder
 {
-    public class Class1
+    public class CRMRestBuilder
     {
     }
 }
---
+++ CRMRestBuilder/CreateAccount.js
@@ -0,0 +1,31 @@
+﻿function CreateAccount() {
+    var entity = {};
+    entity.accountnumber = "1";
+    entity.emailaddress1 = "info@codec.ie";
+    entity.telephone1 = "+353 (0)1 603 4300";
+    entity.name = "Codec-dss";
+    entity.address1_line1 = "Hyde House";
+    entity.address1_line2 = "Adelaide Street";
+    entity.address1_line3 = "Dublin 2";
+
+    var req = new XMLHttpRequest();
+    req.open("POST", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/accounts", true);
+    req.setRequestHeader("OData-MaxVersion", "4.0");
+    req.setRequestHeader("OData-Version", "4.0");
+    req.setRequestHeader("Accept", "application/json");
+    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    req.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            req.onreadystatechange = null;
+            if (this.status === 204) {
+                var uri = this.getResponseHeader("OData-EntityId");
+                var regExp = /\(([^)]+)\)/;
+                var matches = regExp.exec(uri);
+                var newEntityId = matches[1];
+            } else {
+                Xrm.Utility.alertDialog(this.statusText);
+            }
+        }
+    };
+    req.send(JSON.stringify(entity));
+}
